0d424439966d375f7117cac0a71a53f390099484
MARCOS ROBERTO SOARES OLIVEIRA <marcosoaresdo@gmail.com>
2024-03-05 09:32:42 -0300

    atualização   js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function carregar(){
   var msg = document.getElementById("msg");
   var img = document.getElementById("imagem");
   var data = new Date()
-  //var hora = data.getHours()
-  var hora = 17
+  var hora = data.getHours()
+  //var hora = 17
   msg.innerHTML = ` Agora são ${hora} horas`
 
   if(hora >= 0  && hora < 12){
